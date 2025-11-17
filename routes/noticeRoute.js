a0d58a924b6c0df6c8d0fa51487698715a7cb5fb
Apurbaroy01 <apurbaroy00011@gmail.com>
2025-11-17 12:39:24 +0600

    32th

--- routes/noticeRoute.js
+++ routes/noticeRoute.js
@@ -11,19 +11,23 @@ module.exports = function (noticeCollection) {
             .sort({ date: -1 })
             .toArray();
 
-        res.json(notices);
+        res.send(notices);
     });
 
     // POST: Add notice
     app.post("/api/notices", async (req, res) => {
-        const newNotice = {
-            message: req.body.message,
-            date: new Date()
-        };
+        try {
+            const newNotice = req.body;
+            newNotice.date = new Date(); 
 
-        const result = await noticeCollection.insertOne(newNotice);
-        res.json(result);
+            const result = await noticeCollection.insertOne(newNotice);
+            res.status(201).send(result);
+        } catch (err) {
+            console.error(err);
+            res.status(500).send({ message: "Failed to add notice" });
+        }
     });
+
 
     // DELETE: Remove notice
     app.delete("/api/notices/:id", async (req, res) => {
@@ -31,7 +35,7 @@ module.exports = function (noticeCollection) {
             _id: new ObjectId(req.params.id)
         });
 
-        res.json(result);
+        res.send(result);
     });
 
     return app;
